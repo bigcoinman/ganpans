d91e1e63f7f0d12cc4246e693941e3403066c5c7
제이슨 변 <whitebiny@gmail.com>
2026-08-12 02:43:52 +0900

    fix: optimize mobile redirect to clean URL /app and bump service worker cache v33

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'ganpan-support-v32';
+const CACHE_NAME = 'ganpan-support-v33';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './dashboard.html',
   './app.html',
+  './app',
   './style.css',
   './app.css',
   './script.js',
